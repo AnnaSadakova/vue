2104dd48fc9e09a225a76e24300df829f890edcc
AnnaSadakova <162958637+AnnaSadakova@users.noreply.github.com>
2024-04-26 15:28:11 +0300

    vue

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="v-catalog-item">
+    <img
+      class="v-catalog-item-image"
+      :src="'../public/images/' + product_data.image"
+      alt="pic" />
+    <p class="v-catalog-item-name">{{ product_data.name }}</p>
+    <p class="v-catalog-item-price">{{ product_data.price }}</p>
+    <button class="v-catalog-item-add_to_card_btn" @click="SendDataToParent()">
+      add
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-catalog-item",
+  props: {
+    product_data: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    SendDataToParent() {
+      this.$emit("addToCart", this.product_data.article);
+    },
+    }
+};
+</script>
+
+<style lang="scss">
+.v-catalog-item {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 grey;
+  padding: 20px;
+  margin: 20%;
+  &__img {
+    max-width: 100px;
+  }
+}
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="v-main-wrapper">
+    <vCatalog />
+    <vCard />
+  </div>
+</template>
+
+<script>
+import vCatalog from "./v-catalog.vue";
+import vCart from "./v-cart.vue";
+
+export default {
+  name: "v-main-wrapper",
+  components: { vCatalog, vCart },
+  props: {},
+  data() {
+    return {
+      title: "Hello World",
+    };
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  mounted() {
+    console.log("Hello");
+  },
+};
+</script>
+
+<style lang="scss">
+.v-main-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+  & {
+    display: flex;
+  }
+}
+</style>
